cd16ea18b0e639e66284bcd1b2426cfa3a22e708
3326006054 <138670344+3326006054@users.noreply.github.com>
2024-06-20 19:23:11 +0800

    磁盘调度算法.cpp

--- 磁盘调度算法.cpp
+++ 磁盘调度算法.cpp
@@ -1,4 +1,4 @@
-﻿// 磁盘调度算法.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 磁盘调度算法.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
@@ -14,9 +14,9 @@ struct dispatch {
     std::vector<int> order;
     std::vector<bool> unfind;
     std::vector<int> distance;
-    static void init(dispatch* self) {
+    void init() {
         std::cout << "输入起始点" << std::endl;
-        std::cin >> self->start;
+        std::cin >> this->start;
         getchar();
         std::cout << "输入访问序列" << std::endl;
         std::string line;
@@ -24,135 +24,145 @@ struct dispatch {
         std::istringstream iss(line);
         int num;
         while (iss >> num) {
-            self->order.push_back(num);
+            this->order.push_back(num);
         }
-        for (int i = 0; i < self->order.size(); i++) {
-            self->unfind.push_back(true);
+        for (int i = 0; i < this->order.size(); i++) {
+            this->unfind.push_back(true);
         }
     }
-    void FIFO(dispatch* d) {
-        d->distance.clear();
-        for (int i = 0; i < d->unfind.size(); i++) {
-            d->unfind[i] = true;
+    void FIFO() {
+        std::cout << "FIFO" << std::endl;
+        this->distance.clear();
+        for (int i = 0; i < this->unfind.size(); i++) {
+            this->unfind[i] = true;
         }
-        int point = d->start;
+        int point = this->start;
         double distance = 0;
-        for (int i = 0; i < d->order.size(); i++) {
-            distance += std::abs(d->order[i] - point);
-            d->distance[i] = std::abs(d->order[i] - point);
-            std::cout << d->order[i] << " ";
-            point = d->order[i];
+        for (int i = 0; i < this->order.size(); i++) {
+            distance += std::abs(this->order[i] - point);
+            this->distance.push_back(std::abs(this->order[i] - point));
+            std::cout << this->order[i] << " ";
+            point = this->order[i];
         }
         std::cout << std::endl;
-        d->avg_distance = distance / d->order.size();
+        this->avg_distance = distance / this->order.size();
+        std::cout << "平均寻道时间" << std::endl;
+        std::cout << this->avg_distance << std::endl;
     }
-    void SSTF(dispatch* d) {
-        d->distance.clear();
-        for (int i = 0; i < d->unfind.size(); i++) {
-            d->unfind[i] = true;
+    void SSTF() {
+        std::cout << "SSTF" << std::endl;
+        this->distance.clear();
+        for (int i = 0; i < this->unfind.size(); i++) {
+            this->unfind[i] = true;
         }
-        int unfind_num = d->order.size();
+        int unfind_num = this->order.size();
         int found_num = 0;
-        int point = d->start;
+        int point = this->start;
         while (found_num < unfind_num) {
             //寻找最短磁道
             int distance_min = MAXINT;
             int min_num = -1;
-            for (int i = 0; i < d->order.size(); i++) {
-                if (!d->unfind[i]) continue;
-                if (std::abs(d->order[i] - point) < distance_min) {
-                    distance_min = std::abs(d->order[i] - point);
+            for (int i = 0; i < this->order.size(); i++) {
+                if (!this->unfind[i]) continue;
+                if (std::abs(this->order[i] - point) < distance_min) {
+                    distance_min = std::abs(this->order[i] - point);
                     min_num = i;
                 }
             }
-            point = d->order[min_num];
-            d->distance.push_back(distance_min);
-            d->unfind[min_num] = false;
-            std::cout << d->order[min_num] << " ";
+            point = this->order[min_num];
+            this->distance.push_back(distance_min);
+            this->unfind[min_num] = false;
+            std::cout << this->order[min_num] << " ";
             found_num++;//计数加1
         }
         double distance_avg = 0;
-        for (int i = 0; i < d->distance.size(); i++) {
-            distance_avg += d->distance[i];
+        for (int i = 0; i < this->distance.size(); i++) {
+            distance_avg += this->distance[i];
         }
-        d->avg_distance = distance_avg / d->distance.size();
+        this->avg_distance = distance_avg / this->distance.size();
+        std::cout << std::endl;
         std::cout << "平均寻道距离" << std::endl;
-        std::cout << d->avg_distance << std::endl;
+        std::cout << this->avg_distance << std::endl;
     }
-    void SCAN(dispatch* d) {
-        d->distance.clear();
-        std::sort(d->order.begin(), d->order.end());
-        int point = d->start;
+    void SCAN() {
+        std::cout << "SCAN" << std::endl;
+        this->distance.clear();
+        std::sort(this->order.begin(), this->order.end());
+        int point = this->start;
         int begin = 0;
-        for (int i = 0; i < d->order.size(); i++) {
-            if (d->order[i] > point) {
+        for (int i = 0; i < this->order.size(); i++) {
+            if (this->order[i] > point) {
                 begin = i;
                 break;
             }
         }
-        for (int i = begin; i < d->order.size(); i++) {
-            d->distance.push_back(std::abs(d->order[i] - point));
-            point = d->order[i];
-            std::cout << d->order[i] << " ";
+        for (int i = begin; i < this->order.size(); i++) {
+            this->distance.push_back(std::abs(this->order[i] - point));
+            point = this->order[i];
+            std::cout << this->order[i] << " ";
         }
         for (int i = begin - 1; i >= 0; i--) {
-            d->distance.push_back(std::abs(d->order[i] - point));
-            point = d->order[i];
-            std::cout << d->order[i] << " ";
+            this->distance.push_back(std::abs(this->order[i] - point));
+            point = this->order[i];
+            std::cout << this->order[i] << " ";
         }
         double distance_avg = 0;
-        for (int i = 0; i < d->distance.size(); i++) {
-            distance_avg += d->distance[i];
+        for (int i = 0; i < this->distance.size(); i++) {
+            distance_avg += this->distance[i];
         }
-        d->avg_distance = distance_avg / d->distance.size();
+        this->avg_distance = distance_avg / this->distance.size();
         std::cout << std::endl << "平均寻道距离" << std::endl;
-        std::cout << d->avg_distance << std::endl;
+        std::cout << this->avg_distance << std::endl;
     }
-    void C_SCAN(dispatch * d, bool direction) {
-        d->distance.clear();
-        int point = d->start;
+    void C_SCAN(bool direction) {
+        std::cout << "C_SCAN" << std::endl;
+        this->distance.clear();
+        int point = this->start;
         int begin = 0;
         if (direction) {
-            std::sort(d->order.begin(), d->order.end());
-            for (int i = 0; i < d->order.size(); i++) {
-                if (d->order[i] > point) {
+            std::sort(this->order.begin(), this->order.end());
+            for (int i = 0; i < this->order.size(); i++) {
+                if (this->order[i] > point) {
                     begin = i;
                     break;
                 }
             }
         }
         else {
-            std::sort(d->order.begin(), d->order.end(), std::greater<int>());
-            for (int i = 0; i < d->order.size(); i++) {
-                if (d->order[i] < point) {
+            std::sort(this->order.begin(), this->order.end(), std::greater<int>());
+            for (int i = 0; i < this->order.size(); i++) {
+                if (this->order[i] < point) {
                     begin = i;
                     break;
                 }
             }
         }
-        for (int i = begin; i < d->order.size(); i++) {
-            d->distance.push_back(std::abs(d->order[i] - point));
-            point = d->order[i];
-            std::cout << d->order[i] << " ";
+        for (int i = begin; i < this->order.size(); i++) {
+            this->distance.push_back(std::abs(this->order[i] - point));
+            point = this->order[i];
+            std::cout << this->order[i] << " ";
         }
         for (int i = 0; i < begin; i++) {
-            d->distance.push_back(std::abs(d->order[i] - point));
-            point = d->order[i];
-            std::cout << d->order[i] << " ";
+            this->distance.push_back(std::abs(this->order[i] - point));
+            point = this->order[i];
+            std::cout << this->order[i] << " ";
         }
         double distance_avg = 0;
-        for (int i = 0; i < d->distance.size(); i++) {
-            distance_avg += d->distance[i];
+        for (int i = 0; i < this->distance.size(); i++) {
+            distance_avg += this->distance[i];
         }
-        d->avg_distance = distance_avg / d->distance.size();
+        this->avg_distance = distance_avg / this->distance.size();
         std::cout << std::endl << "平均寻道距离" << std::endl;
-        std::cout << d->avg_distance << std::endl;
+        std::cout << this->avg_distance << std::endl;
     }
 };
 int main()
 {
     dispatch d;
-    d.init(&d);
-    d.C_SCAN(&d, false);
+    d.init();
+    d.FIFO();
+    d.SSTF();
+    d.SCAN();
+    d.C_SCAN(false);
 }
 
