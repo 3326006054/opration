aca5a8057c34029328fdcce16ca350668a3cc1d9
3326006054 <138670344+3326006054@users.noreply.github.com>
2024-06-21 13:18:39 +0800

    银行家算法Pro.cpp

--- 银行家算法.cpp
+++ 银行家算法.cpp
@@ -1,176 +1,168 @@
-﻿// 银行家算法.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-
-
-#include <stdio.h>
+// 银行家算法.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+struct Banker {
+    int process_num;
+    int m;
+    int* Resource;
+    int* Available;
+    int** Allocation;
+    int** Need;
+    void init() {
+        std::cout << "输入进程数量" << std::endl;
+        std::cin >> this->process_num;
+        getchar();
+        this->Allocation = new int* [this->process_num];
+        this->Need = new int* [this->process_num];
+        std::cout << "输入Allocation" << std::endl;
+        int m = 0;
+        for (int i = 0; i < this->process_num; i++) {
+            std::string line;
+            std::getline(std::cin, line);
+            std::istringstream count(line);
+            std::istringstream iss(line);
+            int num;
+            if (i == 0) {//分配空间
+                for (m = 0; count >> num; m++)
+                {
+
+                }
+                this->m = m;
+                this->Available = new int[this->m];
+                for (int n = 0; n < this->process_num; n++) {
+                    this->Allocation[n] = new int[this->m];
+                    this->Need[n] = new int[this->m];
+                }
+            }
+            for (m = 0; iss >> num; m++) {
+                this->Allocation[i][m] = num;
+            }
+        }
+        std::cout << "输入Need矩阵" << std::endl;
+        for (int i = 0; i < this->process_num; i++) {
+            std::string line;
+            std::getline(std::cin, line);
+            std::istringstream iss(line);
+            int num;
+            for (int m = 0; iss >> num; m++) {
+                this->Need[i][m] = num;
+            }
+        }
+        std::cout << "输入Available矩阵" << std::endl;
+        for (int i = 0; i < this->m; i++) {
+            std::cin >> this->Available[i];
+        }
+        this->show();
+    }
+    inline void show() {
+        for (int i = 0; i < this->process_num; i++) {
+            for (int j = 0; j < this->m; j++)std::cout << this->Allocation[i][j] << " ";
+            std::cout << "         ";
+            for (int j = 0; j < this->m; j++)std::cout << this->Need[i][j] << " ";
+            std::cout << "         ";
+            if (i == 0)for (int j = 0; j < this->m; j++)std::cout << this->Available[j] << " ";
+            std::cout << std::endl;
+        }
+    }
+    bool safe_check() {
+        int** Allocation = new int*[this->process_num];
+        int* Available = new int[this->m];
+        for (int i = 0; i < this->process_num; i++) {
+            Allocation[i] = new int[this->m];
+            std::copy_n(this->Allocation[i],this->m, Allocation[i]);
+        }
+        std::copy_n(this->Available, this->m, Available);
+        int* found = new int[this->process_num];
+        for (int i = 0; i < this->process_num; i++) found[i] = 0;
+        int found_num = 0;
+        int* safe_order = new int[this->process_num];
+        while (found_num < this->process_num) {
+            int check = 1;
+            for (int i = 0; i < this->process_num; i++) {
+                if (found[i])continue;
+                for (int j = 0; j < this->m; j++) {
+                    if (Available[j] < this->Need[i][j]) {
+                        break;
+                    }
+                    if (j == this->m - 1) {
+                        found[i] = 1;
+                        check = 0;
+                        safe_order[found_num] = i;
+                        for (int m = 0; m < this->m; m++) {
+                            Available[m] += Allocation[i][m];//回收资源
+                        }
+                        found_num++;
+                    }
+                }
+                if (i == this->process_num - 1 && check) {
+                    std::cout << "此状态不安全" << std::endl;
+                    /* 内存回收 */
+                    delete Available;
+                    delete safe_order;
+                    delete found;
+                    for (int n = 0; n < this->process_num; n++) {
+                        delete Allocation[n];
+                    }
+                    delete Allocation;
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < found_num; i++)std::cout << safe_order[i] << " ";
+        std::cout << std::endl;
+        /* 内存回收 */
+        delete Available;
+        delete safe_order;
+        delete found;
+        for (int n = 0; n < this->process_num; n++) {
+            delete Allocation[n];
+        }
+        delete Allocation;
+        return true;
+    }
+    void request() {
+        std::cout << "输入请求" << std::endl;
+        int pid;
+        std::cin >> pid;
+        getchar();
+        int* request = new int[this->m];
+        for (int i = 0; i < this->m; i++) {
+            std::cin >> request[i];
+            if (request[i] > this->Available[i]) {
+                std::cout << "请求的资源不足" << std::endl;
+                return;
+            }
+            if (request[i] > this->Need[pid][i]) {
+                std::cout << "请求的资源过多" << std::endl;
+                return;
+            } 
+        }
+        //尝试分配
+        for (int i = 0; i < this->m; i++) {
+            this->Allocation[pid][i] += request[i];
+            this->Need[pid][i] -= request[i];
+            this->Available[i] -= request[i];
+        }
+        if (!this->safe_check()) {
+            //状态不安全，还原之前状态
+            std::cout << "还原" << std::endl;
+            for (int i = 0; i < this->m; i++) {
+                this->Allocation[pid][i] -= request[i];
+                this->Need[pid][i] += request[i];
+                this->Available[i] += request[i];
+            }
+        }
+        else std::cout << "分配成功" << std::endl;
+        delete request;
+        this->show();
+    }
+};
 int main()
 {
-    int m = 0;//资源类型的数量
-    int n = 0;//进程的数量
-    printf("输入资源类型数");
-    scanf_s("%d", &m);
-    printf("输入进程数");
-    scanf_s("%d", &n);
-    int* resource = new int[m];
-    int* available = new int[m];
-    int** need = new int* [n];
-    int** Allocation = new int* [n];
-    for (int i = 0; i < n; i++)
-    {
-        need[i] = new int[m];
-        Allocation[i] = new int[m];
-    }
-    std::cout << "输入Allocation矩阵" << std::endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            std::cin >> Allocation[i][j];
-        }
-    }
-
-    std::cout << "输入need矩阵" << std::endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            std::cin >> need[i][j];
-        }
-    }
-    std::cout << "输入available数组" << std::endl;
-    for (int i = 0; i < m; i++) {
-        std::cin >> available[i];
-    }
-    //计算resource数组
-    for (int j = 0; j < m; j++) {
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            count += Allocation[i][j];
-        }
-        resource[j] = count + available[j];
-    }
-    std::cout << "Allocation" << std::endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            std::cout << Allocation[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "need" << std::endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            std::cout << need[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "available" << std::endl;
-    for (int i = 0; i < m; i++) {
-        std::cout << available[i] << " ";
-    }
-    //保存当前状态
-    int pid = 0;
-    int* require = new int[m];
-    int** Allocation_0 = new int* [n];
-    int** need_0 = new int* [n];
-    int* available_0 = new int[m];
-    for (int i = 0; i < n; i++) {
-        Allocation_0[i] = new int[m];
-        need_0[i] = new int[m];
-        for (int j = 0; j < m; j++) {
-            Allocation_0[i][j] = Allocation[i][j];
-            need_0[i][j] = need[i][j];
-        }
-    }
-    for (int i = 0; i < m; i++) {
-        available_0[i] = available[i];
-    }
-    while (1) {
-        std::cout << "输入进程 请求" << std::endl;
-        std::cin >> pid;
-        for (int i = 0; i < m; i++) {
-            std::cin >> require[i];
-        }
-        int available_judge = 0;//是否可以分配
-        int plethora_judge = 0;//请求的资源大于need
-        //进行判断
-        //判断available数组是否有空余资源可以分配
-        for (int i = 0; i < m; i++) {
-            if (require[i] > available[i]) {
-                std::cout << "可用资源不足" << std::endl;
-                available_judge = 1;
-                break;
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            if (require[i] > need[pid][i]) {
-                std::cout << "请求过多资源" << std::endl;
-                plethora_judge = 1;
-                break;
-            }
-        }
-        if (available_judge) continue;
-        if (plethora_judge) continue;
-        
-        //尝试分配
-        for (int i = 0; i < m; i++) {
-            available[i] -= require[i];
-            need[pid][i] -= require[i];
-            Allocation[pid][i] += require[i];
-        }
-        //寻找安全序列
-        int* find = new int[n];
-        for (int i = 0; i < n; i++) {
-            find[i] = 0;
-        }
-        int end_num = 0;
-        int* safe_order = new int[n];
-        int unsafe;
-        while (end_num < n) {
-            int success_find = 0;
-            unsafe = 0;
-            for (int i = 0; i < n; i++) {
-                if (find[i]) continue;//判断是否该进程是否结束
-                for (int j = 0; j < m; j++) {
-                    if (available[j] < need[i][j]) {
-                        break;//该进程无法分配，直接寻找下一个进程
-                    }
-                    if (j == (m - 1)) {
-                        for (int k = 0; k < m; k++) {
-                            available[k] += Allocation[i][k];
-                        }
-                        find[i] = 1;
-                        safe_order[end_num] = i;
-                        end_num++;
-                        success_find = 1;
-                    }
-                }
-                if (success_find) break;//本次循环找到了可执行的程序，结束本次循环
-                if (i == (n - 1)) {
-                    std::cout << "无法寻找到安全序列" << std::endl;
-                    unsafe = 1;
-                    break;
-                }
-            }
-            if (unsafe) break;
-
-        }
-        //还原初始状态
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                Allocation[i][j] = Allocation_0[i][j];
-                need[i][j] = need_0[i][j];
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            available[i] = available_0[i];
-        }
-        if (!unsafe) {//如果允许分配，更新数组
-            for (int i = 0; i < n; i++) {
-                std::cout << safe_order[i] << " ";
-            }
-            for (int i = 0; i < m; i++) {
-                available[i] -= require[i];
-                need[pid][i] -= require[i];
-                Allocation[pid][i] += require[i];
-            }
-        }
-        std::cout << std::endl;
-    }
+    Banker b;
+    b.init();
+    b.request();
 }
